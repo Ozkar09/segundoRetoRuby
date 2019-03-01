9e2b9f4a803562e55b7e8a6e39be24e8d2856b6a
Ozkar09 <oscarbeltran99@hotmail.com>
2019-02-28 20:40:54 -0500

    Se supera prueba 2

--- my_solution.rb
+++ my_solution.rb
@@ -1,4 +1,6 @@
 # Simple String Methods
+old_string = "Ruby is cool"
+new_string = old_string.reverse.upcase
 
 # Tu solucion abajo:
 
@@ -24,15 +26,3 @@ describe 'new_string' do
     expect(new_string).to eq "LOOC SI YBUR"
   end
 end
-
-
-# Escriban un progrma que le pregunte al usuario por su primer nombre, luego el apellido y luego que imprima una oracion que diga "Hola <nombre> <apellido> como estas"
-Collapse
-
-
-
-
-Message Input
-
-
-Message Jose Ferney Medina
